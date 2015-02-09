f8ed43c2d2e9db187a7908a6ac5a5b9c0511ccb7
kele <damian.bogel@gmail.com>
2015-02-10 00:11:04 +0100

    TODO: pres

--- Makefile
+++ Makefile
@@ -1,16 +1,16 @@
-POPULATION_SIZE=1000
+POPULATION_SIZE=100
 AGE_THRESHOLD=0.15
 ALPHA_THRESHOLD=0.6
-REITER_THRESEHOLD=5
+REITER_THRESHOLD=20
 
 solver: main.cpp Solver.cpp FixedSet.cpp PermUtils.cpp
-	clang++ -O2 -std=c++1y -o solver *.cpp
+	@clang++ -O2 -std=c++1y -o solver *.cpp
 
 solver_debug: main.cpp Solver.cpp FixedSet.cpp PermUtils.cpp
-	clang++ -g -std=c++1y -o solver_debug *.cpp
+	@clang++ -g -std=c++1y -o solver_debug *.cpp
 
 run40: solver
-	cat wt40.txt | ./solver 40 125 $(POPULATION_SIZE) $(AGE_THRESHOLD) $(ALPHA_THRESHOLD) $(REITER_THRESEHOLD)
+	@cat wt40.txt | ./solver 40 125 $(POPULATION_SIZE) $(AGE_THRESHOLD) $(ALPHA_THRESHOLD) $(REITER_THRESHOLD)
 
 valgrind40: solver_debug 
-	cat wt40.txt | valgrind ./solver_debug 40 125 $(POPULATION_SIZE) $(AGE_THRESHOLD) $(ALPHA_THRESHOLD) $(REITER_THRESEHOLD)
+	@cat wt40.txt | valgrind ./solver_debug 40 125 $(POPULATION_SIZE) $(AGE_THRESHOLD) $(ALPHA_THRESHOLD) $(REITER_THRESHOLD)
--- compare.py
+++ compare.py
@@ -1,16 +1,52 @@
-my = []
-
-with open("my.txt") as x:
-    for line in x:
-        my.append(int(line))
+import subprocess
+from multiprocessing import Pool
 
 opt = []
 with open("wtopt40.txt") as x:
     for line in x:
-        opt.append(int(line))
+        opt.append(int(line) + 1)
 
-for i in range(len(my)):
-    if opt[i] != 0:
-        print(my[i]/opt[i])
-    else:
-        print('abs', my[i] - opt[i])
+age_thresholds = [0.15, 0.40]
+alpha_thresholds = [0.3, 0.6, 0.7, 0.8]
+populs = [40, 100, 200, 400]
+reiters = [1, 5, 10]
+
+args = []
+for age in age_thresholds:
+    for alpha in alpha_thresholds:
+        for p in populs:
+            for r in reiters:
+                args.append((age, alpha, p, r))
+
+
+def run_test(args):
+    age, alpha, population, reiters = args
+    command = [
+        'make', 'run40',
+        'AGE_THRESHOLD=' + str(age),
+        'ALPHA_THRESHOLD=' + str(alpha),
+        'REITER_THRESHOLD=' + str(reiters),
+        'POPULATION_SIZE=' + str(population)]
+
+    my = subprocess.check_output(command).split()
+    my = [int(y) for y in my]
+
+    ratios = []
+    bad_ratios = 0
+    for i in range(125):
+        r = my[i]/opt[i]
+        if r > 10:
+            bad_ratios += 1
+        else:
+            ratios.append(r)
+
+    ratio_avg = 0
+    for r in ratios:
+        ratio_avg += r
+    ratio_avg /= len(ratios)
+
+    print('%f | Age: %f | Alpha: %f | Reiters: %d | Population: %d | Ratio_avg: %f | Bad ratios: %d' % (ratio_avg, age, alpha, reiters, population, ratio_avg, bad_ratios))
+
+
+p = Pool(8)
+p.map(run_test, args)
